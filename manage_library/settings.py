f691c64740ddd5dccd56e33d936d005a14a9d069
MAmin-Sh <a2954794@gmail.com>
2025-09-16 15:06:30 -0700

    second commite

--- manage_library/settings.py
+++ manage_library/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dk5*9(f^$cd7^v%&x*t=)@bewz$kq5jl+)y(2*d%djkmpzau&1'
+# SECRET_KEY = 'django-insecure-dk5*9(f^$cd7^v%&x*t=)@bewz$kq5jl+)y(2*d%djkmpzau&1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -94,10 +96,15 @@ WSGI_APPLICATION = 'manage_library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',     
+        'USER': 'postgres',     
+        'PASSWORD': 'admin',   
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
